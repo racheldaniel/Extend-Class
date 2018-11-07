c710c3abb7adf4cd9f3725d56b64ebc8135cb781
Rachel Daniel <rachdaniel88@gmail.com>
2018-11-07 13:07:07 -0600

    finished in-class challenge

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,21 @@
-import sayHello from "./hello"
-import sayGoodbye from "./goodbye"
-import SandwichMaker from "./sandwichMaker"
+import DOMComponent from "../lib/node_modules/nss-domcomponent/index"
 
-sayHello()
-sayGoodbye()
 
-SandwichMaker.placeOrder("rye", "capicola", "provolone")
+class H1 extends DOMComponent {
+  constructor(attributes, ...children){
+    super("h1", attributes, ...children)
+  }
+}
+
+class Div extends DOMComponent {
+  constructor(attributes, ...children){
+    super("div", attributes, ...children)
+  }
+}
+
+let Heading = new H1({className: "heading", id: "main--heading", textContent:"I love extending classes"} )
+
+let MainDiv = new Div({className: "intro", id: "intro", textContent:"creating new subclasses"}, Heading )
+
+MainDiv.render(".output")
+
